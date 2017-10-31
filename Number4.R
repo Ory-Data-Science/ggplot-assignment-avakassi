8ab62af128bccec8183532e8118fe6cff09e6f37
avakassi <amber.vakassi@stlcop.edu>
2017-10-31 16:21:30 -0500

    4.R

--- Number4.R
+++ Number4.R
@@ -6,3 +6,8 @@ dat <- read.csv(url("http://www.esapubs.org/archive/ecol/E088/096/avian_ssd_jan0
                 sep = "\t", na.strings = c("-999.00", "999.00", "-999", "999"))
 
 #your code goes here
+
+ggplot(data = dat, aes(x = F_mass)) + geom_histogram( fill = "blue",alpha = 0.3) +  labs(x = "Mass(g)") + 
+  geom_histogram(aes(x = M_mass, alpha = 0.3)) + scale_x_log10() + facet_wrap(~family)
+
+ 
